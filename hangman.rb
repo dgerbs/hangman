d2af0d1a79296bd756580a7bfa7963ad8dc2a6b7
Doug Gerber <dojoh@outlook.com>
2013-08-20 16:35:45 -0400

    Edit Hangman

--- hangman.rb
+++ hangman.rb
@@ -7,6 +7,7 @@ puts "\nWELCOME to HANGMAN!\n\n"
 
 words = [ "apple", "xylophone", "zebra", "foobar", "message" ]
 word = words.sample
+guessed = []
 chances = 8
 display = '_' * word.length
 
@@ -17,25 +18,37 @@ loop do
 	print "Guess a single letter (a-z) or the entire word: "
 	response = gets.chomp
 
-	if response.length == 1 && word.include?(response)	
-		# Update word displayed to user
-		puts "\nFound #{word.scan(response).length} occurance(s) of the character #{response}.\n\n"
-		word.length.times do |i|
-			display[i] = response if word[i] == response
+	if response.length == 1
+		if guessed.include?(response)
+			puts "\nYou already guessed that letter, try again.\n\n"
+		elsif	word.include?(response)	
+			# Update word displayed to user
+			guessed << response
+			puts "\nFound #{word.scan(response).length} occurance(s) of the character #{response}.\n\n"
+			word.length.times do |i|
+				display[i] = response if word[i] == response
+			end
+			# If no letter is found
+		else  
+			guessed << response
+			chances -= 1
+			puts "\nSorry, no " + response + "'s found.\n\n"
 		end
-		# If no letter is found
-	elsif response.length == 1 && !word.include?(response) 
-		chances -= 1
-		puts "\nSorry, no " + response + "'s found.\n\n"
-	end
-		# Word is guessed, end game
-	if response == word || display == word
-		print "Congratulations, you've guessed the word!\n\n"
+	else
+		if response == word
+			puts "Congratulations, you've guessed the word!\n"
+		else
+			puts "Sorry, the hidden word was #{word}.\n"
+		end
 		break
 	end
 		# Player runs out of chances, end game
 	if chances <= 0
 		puts "You're out of chances, better luck next time..."
+		break
+	end
+	if display == word
+		puts "Congratulations, you've guessed the word!\n"
 		break
 	end
 end
